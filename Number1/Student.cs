339b69360965666ab20086ea91e374d97bf4002b
Zadok Joshua <65626254+Tsadhoq@users.noreply.github.com>
2022-04-19 12:01:09 +0100

    Update Student.cs

--- Number1/Student.cs
+++ Number1/Student.cs
@@ -1,12 +1,38 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace CCLAcademy
 {
-    class Student
+    class Student : Human, IQuestionFaciliator, IQuestionStudent, IAnswer
     {
+        public ProgramType MyProgram { get; set; }
+        public Student(string name, ProgramType program)
+        {
+            MyProgram = program;
+        }
+        public override void AttendClass(DaysOfWeek classDay)
+        {
+            if(classDay == DaysOfWeek.Tuesday || classDay == DaysOfWeek.Monday || classDay == DaysOfWeek.Thursday && MyProgram == ProgramType.Backend)
+            {
+                Console.WriteLine("The host will let you in shortly");
+                Thread.Sleep(5000);
+                Console.WriteLine("You’re in!");
+            }
+            else if (classDay == DaysOfWeek.Tuesday || classDay == DaysOfWeek.Wednesday || classDay == DaysOfWeek.Thursday && MyProgram == ProgramType.Frontend)
+            {
+                Console.WriteLine("The host will let you in shortly");
+                Thread.Sleep(5000);
+                Console.WriteLine("You’re in!");
+            }
+            else
+            {
+                Console.WriteLine("No class today");
+            }
+        }
     }
 }
+
