9b5f898f06d4c79fd7e824cdd618748d3875cfc6
Zadok Joshua <65626254+Tsadhoq@users.noreply.github.com>
2022-04-19 11:52:52 +0100

    Update Human.cs

--- Number1/Human.cs
+++ Number1/Human.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +8,22 @@ namespace CCLAcademy
 {
     class Human
     {
+        public string Name { get; set; }
+        public string Program { get; set; }
+
+        public virtual string AskQuestion()
+        {
+            return "Excuse me! I have a question...";
+        }
+
+        public virtual string AnswerQuestion()
+        {
+            return "The answer to your question is...";
+        }
+
+        public virtual void AttendClass(DaysOfWeek classDay)
+        {
+
+        }
     }
 }
