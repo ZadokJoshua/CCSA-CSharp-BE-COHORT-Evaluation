651f50a02dde980d68bd948ef7a934d1a2f19ef0
Zadok Joshua <65626254+Tsadhoq@users.noreply.github.com>
2022-04-19 17:06:22 +0100

    Update Program.cs

--- Number1/Program.cs
+++ Number1/Program.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 
 namespace CCLAcademy
 {
@@ -10,20 +6,39 @@ namespace CCLAcademy
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(@"
+         _____  _____  _        ___  _____   ___ ______ ________  ____   __
+        /  __ \/  __ \| |      / _ \/  __ \ / _ \|  _  \  ___|  \/  \ \ / /
+        | /  \/| /  \/| |     / /_\ \ /  \// /_\ \ | | | |__ | .  . |\ V / 
+        | |    | |    | |     |  _  | |    |  _  | | | |  __|| |\/| | \ /  
+        | \__/\| \__/\| |____ | | | | \__/\| | | | |/ /| |___| |  | | | |  
+         \____/ \____/\_____/ \_| |_/\____/\_| |_/___/ \____/\_|  |_/ \_/  
+            ");
+
             Human[] myTest = new Human[10];
 
             myTest[0] = new Student("Zadok", ProgramType.Backend);
-            myTest[1] = new Student("James", ProgramType.Backend);
-            myTest[2] = new Facilitator("Sam", ProgramType.Frontend);
-            myTest[3] = new Student("Sadok", ProgramType.Backend);
-            myTest[4] = new Student("Amek", ProgramType.Backend);
-            myTest[5] = new Facilitator("Pamila", ProgramType.Frontend);
-            myTest[6] = new Student("Nimi", ProgramType.Backend);
-            myTest[7] = new Student("Josh", ProgramType.Backend);
-            myTest[8] = new Facilitator("Edem", ProgramType.Frontend);
-            myTest[9] = new Facilitator("Desmond", ProgramType.Backend);
+            myTest[1] = new Student("James", ProgramType.Frontend);
+            myTest[2] = new Student("Tsado", ProgramType.Backend);
+            myTest[3] = new Student("Amek", ProgramType.Backend);
+            myTest[4] = new Student("Nimi", ProgramType.Frontend);
+            myTest[5] = new Student("Josh", ProgramType.Backend);
 
-            myTest[0].AttendClass(DaysOfWeek.Saturday);
+            myTest[6] = new Facilitator("Edem", ProgramType.Frontend);
+            myTest[7] = new Facilitator("Desmond", ProgramType.Backend);
+            myTest[8] = new Facilitator("Pamila", ProgramType.Frontend);
+            myTest[9] = new Facilitator("Sam", ProgramType.Frontend);
+
+            for (int i = 0; i < myTest.Length; i++)
+            {
+                Console.WriteLine("\t-------------------------------");
+                Console.WriteLine($"\tOn {DaysOfWeek.Monday}...");
+                myTest[i].AttendClass(DaysOfWeek.Monday);
+
+                Console.WriteLine($"\t...and on {DaysOfWeek.Wednesday}");
+                myTest[i].AttendClass(DaysOfWeek.Wednesday);
+                Console.WriteLine("\t-------------------------------");
+            }
         }
     }
 }
