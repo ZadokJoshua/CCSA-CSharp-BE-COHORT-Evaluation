2c37b81599d2f11e6d98e68572794f0cc8e8564d
Zadok Joshua <65626254+Tsadhoq@users.noreply.github.com>
2022-04-19 11:51:56 +0100

    Update Facilitator.cs

--- Number1/Facilitator.cs
+++ Number1/Facilitator.cs
@@ -8,5 +8,31 @@ namespace CCLAcademy
 {
     class Facilitator
     {
+        public ProgramType MyProgram{ get; set; }
+
+        public Facilitator(string name, ProgramType program)
+        {
+            MyProgram = program;
+        }
+
+        public override void AttendClass(DaysOfWeek classDay)
+        {
+            if (classDay == DaysOfWeek.Tuesday || classDay == DaysOfWeek.Monday || classDay == DaysOfWeek.Thursday && MyProgram == ProgramType.Backend)
+            {
+                Console.WriteLine("The host will let you in shortly");
+                Thread.Sleep(5000);
+                Console.WriteLine("You’re in!");
+            }
+            else if(classDay == DaysOfWeek.Tuesday || classDay == DaysOfWeek.Wednesday || classDay == DaysOfWeek.Thursday && MyProgram == ProgramType.Frontend)
+            {
+                Console.WriteLine("The host will let you in shortly");
+                Thread.Sleep(5000);
+                Console.WriteLine("You’re in!");
+            }
+            else
+            {
+                Console.WriteLine("No class today");
+            }
+        }
     }
 }
