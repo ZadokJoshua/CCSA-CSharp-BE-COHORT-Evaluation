1468ff6c51e9b4d1e0c8c17180e6a97a2cdfc912
Zadok Joshua <65626254+Tsadhoq@users.noreply.github.com>
2022-04-19 12:06:27 +0100

    Update DaysOfWeek.cs

--- Number1/DaysOfWeek.cs
+++ Number1/DaysOfWeek.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +6,14 @@ using System.Threading.Tasks;
 
 namespace CCLAcademy
 {
-    class DaysOfWeek
-    {
+    public enum DaysOfWeek
+    { 
+        Sunday,
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday
     }
 }
