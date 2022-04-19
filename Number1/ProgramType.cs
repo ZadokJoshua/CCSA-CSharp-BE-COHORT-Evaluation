c01e3908b6821dd15c459870491b0115e6293b56
Zadok Joshua <65626254+Tsadhoq@users.noreply.github.com>
2022-04-19 12:04:57 +0100

    Update ProgramType.cs

--- Number1/ProgramType.cs
+++ Number1/ProgramType.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +6,9 @@ using System.Threading.Tasks;
 
 namespace CCLAcademy
 {
-    class ProgramType
+    enum ProgramType
     {
+        Backend,
+        Frontend
     }
 }
