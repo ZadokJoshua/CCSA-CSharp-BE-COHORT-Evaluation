afd9a93d3cfc74af27470bc597be2368b13611f8
Zadok Joshua <65626254+Tsadhoq@users.noreply.github.com>
2022-04-19 11:31:21 +0100

    Add files via upload

---
+++ Number2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace MinAndMaxRecusion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get user input
+            Console.Write("Enter the length of the Array: ");
+            int arrLen = Convert.ToInt32(Console.ReadLine());
+
+            // Create array
+            int[] userArray = new int[arrLen];
+
+            Console.WriteLine("Enter the values of the Array: ");
+
+            // For loop
+            for(int i = 0; i < arrLen; i++)
+            {
+                Console.Write($"Element[{i}] = ");
+                userArray[i] = int.Parse(Console.ReadLine());
+            }
+
+
+            Console.WriteLine("------------------------");
+            Console.WriteLine("Maximum value = " + MaxValue(userArray, arrLen));
+            Console.WriteLine("Minimum value = " + MinValue(userArray, arrLen));
+        }
+
+        public static int MinValue(int[] numsArr, int arraylength)
+        {
+            if(arraylength == 1)
+            {
+                return numsArr[0];
+            }
+            else
+            {
+                return Math.Min(numsArr[arraylength - 1], MinValue(numsArr, arraylength - 1));
+            }
+        }
+
+        public static int MaxValue(int[] numsArr, int arraylength)
+        {
+            if (arraylength == 1)
+            {
+                return numsArr[0];
+            }
+            else
+            {
+                return Math.Max(numsArr[arraylength - 1], MaxValue(numsArr, arraylength - 1));
+            }
+        }
+
+    }
+}
---
+++ Number2/Program1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace FindNumbersInAString
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Get user input
+            Console.Write("Enter the String: ");
+            string theString = Console.ReadLine();
+
+            // Call method
+            Console.WriteLine(NumberFinder(theString));
+        }
+
+        // Method
+        public static string NumberFinder(string thisString)
+        {
+            char[] characters = thisString.ToCharArray();
+            int letters = 0;
+            int numbers = 0;
+
+            for (int i = 0; i < characters.Length; i ++)
+            {
+                if (Char.IsDigit(characters[i]))
+                {
+                    numbers++;
+                }
+                else
+                {
+                    letters++;
+                }
+            }
+
+            return $"Text characters: {letters}\nNumeric characters: {numbers}";
+        }
+    }
+}
