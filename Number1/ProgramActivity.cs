96d6a4a5e3ae388297e5898292143fd2912861db
Zadok Joshua <65626254+Tsadhoq@users.noreply.github.com>
2022-04-19 12:03:12 +0100

    Update ProgramActivity.cs

--- Number1/ProgramActivity.cs
+++ Number1/ProgramActivity.cs
@@ -1,12 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 
 namespace CCLAcademy
 {
-    class ProgramActivity
+    interface IQuestionStudent
     {
+
+    }
+
+    interface IQuestionFaciliator
+    {
+        
+    }
+
+    interface IAnswer
+    {
+
     }
 }
+
